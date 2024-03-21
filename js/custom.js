fd5947f4096006d0fc18cd704bde1cc51a6e7a4f
Maria <maria123321borovikova@gmail.com>
2024-03-21 12:01:03 +0100

    main

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,23 @@
+(function ($) {
 
-  (function ($) {
-  
   "use strict";
 
-    // NAVBAR
-    $('.navbar-nav .nav-link').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
-    
-  })(window.jQuery);
+  // NAVBAR
+  $('.navbar-nav .nav-link').click(function(){
+    $(".navbar-collapse").collapse('hide');
+  });
+  
+})(window.jQuery);
+
+import { onWebVital } from 'web-vitals';
+
+function sendToAnalytics({ name, value, id }) {
+  const body = JSON.stringify({ name, value, id });
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon('https://your-analytics-endpoint.com/report', body);
+  } else {
+    fetch('https://your-analytics-endpoint.com/report', { body, method: 'POST', keepalive: true });
+  }
+}
+
+onWebVital(sendToAnalytics);
