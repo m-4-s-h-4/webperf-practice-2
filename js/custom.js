3a9b09da0912a384f90d9dbf7a147e7ffa319516
Maria <maria123321borovikova@gmail.com>
2024-03-22 12:13:37 +0100

    main

--- js/custom.js
+++ js/custom.js
@@ -3,21 +3,49 @@
   "use strict";
 
   // NAVBAR
-  $('.navbar-nav .nav-link').click(function(){
-    $(".navbar-collapse").collapse('hide');
-  });
-  
+  $(document).ready(function() {
+    $('.navbar-nav .nav-link').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    // geolocation
+    function showGeolocationBanner() {
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                $('#geolocation-banner').text(`Location detected: Lat ${position.coords.latitude}, Lon ${position.coords.longitude}`).show();
+            });
+        }
+    }
+
+    //status 
+    function updateOnlineStatus() {
+        const banner = $('#online-status-banner');
+        if (navigator.onLine) {
+            banner.text("You are online").show();
+        } else {
+            banner.text("You are offline").show();
+        }
+    }
+
+    window.addEventListener('online', updateOnlineStatus);
+    window.addEventListener('offline', updateOnlineStatus);
+
+    //initial status
+    updateOnlineStatus();
+
+    //geolocation information
+    showGeolocationBanner();
+
+    //notification 
+    setTimeout(() => {
+        console.log('This is a simulated push notification!');
+        Notification.requestPermission().then(permission => {
+            if (permission === "granted") {
+                new Notification("This is a push notification");
+            }
+        });
+    }, 15000);
+});
+
+
 })(window.jQuery);
-
-import { onWebVital } from 'web-vitals';
-
-function sendToAnalytics({ name, value, id }) {
-  const body = JSON.stringify({ name, value, id });
-  if (navigator.sendBeacon) {
-    navigator.sendBeacon('https://your-analytics-endpoint.com/report', body);
-  } else {
-    fetch('https://your-analytics-endpoint.com/report', { body, method: 'POST', keepalive: true });
-  }
-}
-
-onWebVital(sendToAnalytics);
