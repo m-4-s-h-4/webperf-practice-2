893659de4c6d1741de3906c98ded72c8a5cef559
Maria <maria123321borovikova@gmail.com>
2024-03-27 19:41:38 +0100

    main

---
+++ nextjs-crispy-chicken/src/app/layout.tsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+}
