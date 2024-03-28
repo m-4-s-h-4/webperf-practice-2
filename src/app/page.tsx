61e83eeb2900aa8b6c44449488af0b0b034a5a21
Maria <maria123321borovikova@gmail.com>
2024-03-28 10:16:09 +0100

    went back to "removed files"

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Main from "./components/Main";
+import "./globals.css";
+import "./css/bootstrap-icons.css";
+import "./css/bootstrap.min.css";
+
+export default function Home() {
+  return <Main />;
+}
